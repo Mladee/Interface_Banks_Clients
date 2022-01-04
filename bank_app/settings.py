f2ca0dc986c7b2681862703bb6e44cead34039e1
Mlade <andreimladenovici@yahoo.ro>
2022-01-04 23:32:38 +0200

    Update settings.py

--- bank_app/settings.py
+++ bank_app/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bank-clients-database.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
